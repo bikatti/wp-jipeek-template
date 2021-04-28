869a3027000ec72b4f15f9bdaf28b82556720fc5
SebasTyler <ibikatti@gmail.com>
2021-04-28 17:04:44 -0400

    Add Css Gutenberg tables and quotes

--- functions.php
+++ functions.php
@@ -53,7 +53,7 @@ function assets() {
 add_action( 'wp_enqueue_scripts', 'assets' );
 
 function site_block_editor_css() {
-    $ver = '1.0.1';
+    $ver = '1.0.2';
     wp_register_style( 'roboto', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', '', $ver , 'all' );
 
     wp_register_style( 'editor', get_template_directory_uri( ).'/editor-styles.css', '', $ver, 'all' );
